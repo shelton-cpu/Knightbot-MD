dc02204c6b37a6d3a256b8f3ee5ee2733f63c78f
shelton-cpu <155022103+shelton-cpu@users.noreply.github.com>
2025-02-19 14:54:20 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Megas-Top",
+  botOwner: 'S.R.C', // Your name
+  ownerNumber: '258875349505', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Comprem megas.",
   version: "1.0.0",
 };
 
